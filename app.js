6064987fb64882fc4c6d3d947501bf8c3cf287d0
JatinMehra <mehrajatin142002@gmail.com>
2022-06-23 12:22:42 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.listen(5000, function(){
+    console.log("Server started at port 5000");
+});
